ee5c797e253a5f1b4189b0d7fe773a5b619e9e58
jtmclucas13 <jtmclucas13@gmail.com>
2018-01-20 15:07:35 -0500

    Initialize client-side routing with react-router-dom and a single route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { PatientList } from './views';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <BrowserRouter>
+            <div>
+                <h1>Patient Details Hub</h1>
+                <Route exact path="/" component={PatientList} />
+            </div>
+        </BrowserRouter>
     );
   }
 }
-
-export default App;
---
+++ src/views/patient-list/patient-list.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class PatientList extends Component {
+    static propTypes = {
+    };
+
+    render() {
+        return (
+            <div>PatientList</div>
+        );
+    }
+}
